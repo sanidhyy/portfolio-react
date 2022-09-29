1caf2df803e68378b3a9deb4f054a28be0a126c5
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-09-29 16:04:27 +0530

    backend data fetch client created

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,34 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { images } from "../../constants";
-
+import { urlFor, client } from "../../client";
 import "./About.scss";
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I'm a good web developer",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Design",
-    description: "I'm a good web developer",
-    imgUrl: images.about02,
-  },
-  {
-    title: "UI/UX",
-    description: "I'm a good web developer",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Web Animations",
-    description: "I'm a good web developer",
-    imgUrl: images.about04,
-  },
-];
-
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -46,7 +30,7 @@ const About = () => {
             className="app__profiles-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
