d775d56594fc4e6396a205ae33f465488e817b94
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-09-30 21:49:48 +0530

    broken link fixed

--- frontend-react/src/components/NavigationDots.jsx
+++ frontend-react/src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "skills", "testimonials", "contact"].map(
+      {["home", "about", "work", "skills", "testimonial", "contact"].map(
         (item, index) => (
           <a
             href={`#${item}`}
